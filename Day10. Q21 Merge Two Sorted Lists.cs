15b656f1b0a43ed45b79cf679b807e640e59a940
PENG HE <hopkinau@gmail.com>
2023-08-29 23:36:27 +1000

    Q21-link list

---
+++ Day10. Q21 Merge Two Sorted Lists.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode p = dummy;
+        ListNode p1 = list1, p2 = list2;
+
+        while (p1 != null && p2 != null) {
+            // Compare the values of p1 and p2
+            // Attach the node with the smaller value to the p pointer
+            if (p1.val > p2.val) {
+                p.next = p2;
+                p2 = p2.next;
+            } else {
+                p.next = p1;
+                p1 = p1.next;
+            }
+            // Move the p pointer forward
+            p = p.next;
+        }
+
+        if (p1 != null) {
+            p.next = p1;
+        }
+
+        if (p2 != null) {
+            p.next = p2;
+        }
+
+        return dummy.next;
+    }
+}

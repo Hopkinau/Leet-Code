40b8d18ae2604960523b986f2d8227299d4f5184
PENG HE <hopkinau@gmail.com>
2023-09-06 13:49:08 +1000

    Update Day16-Q121-Best Time to Buy and Sell Stock.cs

--- Array/Day16-Q121-Best Time to Buy and Sell Stock.cs
+++ Array/Day16-Q121-Best Time to Buy and Sell Stock.cs
@@ -27,16 +27,17 @@ public class Solution {
 }
 // If you input the array `prices = [7,1,5,3,6,4]` into the `MaxProfit` function, it will calculate the maximum profit that can be obtained from buying and selling stocks with these prices. Let's walk through the calculation step by step:
 
-// 1. Initialize `buy` to the first element of the `prices` array: `buy = 7`.
-// 2. Initialize `profit` to 0.
-// 3. Start iterating through the array from the second element (`i = 1`).
+// Initialize buy to the first element of the prices array: buy = 7.
 
-//    - At `i = 1`, `buy` (7) is not greater than `prices[1]` (1), so we calculate the potential profit: `prices[1] - buy = 1 - 7 = -6`. Since the profit is negative, we don't update `profit`.
-//    - At `i = 2`, `buy` (7) is less than `prices[2]` (5), so we update `buy` to 5, and set `prices[2]` to 0 to mark it as a purchase.
-//    - At `i = 3`, `buy` (5) is not greater than `prices[3]` (3), so we calculate the potential profit: `prices[3] - buy = 3 - 5 = -2`. Since the profit is negative, we don't update `profit`.
-//    - At `i = 4`, `buy` (5) is less than `prices[4]` (6), so we calculate the potential profit: `prices[4] - buy = 6 - 5 = 1`. We update `profit` to 1 because it's the maximum profit seen so far.
-//    - At `i = 5`, `buy` (5) is less than `prices[5]` (4), so we calculate the potential profit: `prices[5] - buy = 4 - 5 = -1`. Since the profit is negative, we don't update `profit`.
+// Initialize profit to 0.
 
-// 4. After the loop finishes, the `profit` variable holds the maximum profit, which is 1.
+// Start iterating through the array from the second element (i = 1).
 
-// So, for the input `prices = [7,1,5,3,6,4]`, the `MaxProfit` function returns a maximum profit of 1, which means the best strategy is to buy at a price of 5 and sell at a price of 6 to achieve a profit of 1.
+// At i = 1, buy (7) is greater than prices[1] (1), so we update buy to 1, and set prices[1] to 0 to mark it as a purchase.
+// At i = 2, buy (1) is less than prices[2] (5), so we calculate the potential profit: prices[2] - buy = 5 - 1 = 4. We update profit to 4 because it's the maximum profit seen so far.
+// At i = 3, buy (1) is less than prices[3] (3), so we calculate the potential profit: prices[3] - buy = 3 - 1 = 2. We update profit to 4 because it's still the maximum profit seen so far.
+// At i = 4, buy (1) is less than prices[4] (6), so we calculate the potential profit: prices[4] - buy = 6 - 1 = 5. We update profit to 5 because it's the maximum profit seen so far.
+// At i = 5, buy (1) is less than prices[5] (4), so we calculate the potential profit: prices[5] - buy = 4 - 1 = 3. We update profit to 5 because it's still the maximum profit seen so far.
+// After the loop finishes, the profit variable holds the correct maximum profit, which is 5.
+
+// So, for the input prices = [7,1,5,3,6,4], the maximum profit is indeed 5, achieved by buying at a price of 1 and selling at a price of 6.

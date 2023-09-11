6d562e500bc7a7ac74403345f85c474599b97497
PENG HE <hopkinau@gmail.com>
2023-09-11 14:31:29 +1000

    Create Day19-Q283 Move Zeroes.cs

---
+++ Array/Day19-Q283 Move Zeroes.cs
@@ -0,0 +1,18 @@
+https://www.youtube.com/watch?v=k5lIW5XxC7g
+
+public class Solution {
+    public void ReverseString(char[] s) {
+        int left = 0;
+        int right = s.Length - 1;
+
+        while (left < right) {
+            // Swap characters at left and right indices
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}

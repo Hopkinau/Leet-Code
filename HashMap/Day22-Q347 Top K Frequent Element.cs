4904051b8c6d1fd6120397ec13d35af86015e25a
PENG HE <hopkinau@gmail.com>
2023-09-18 23:22:02 +1000

    Update Day22-Q347 Top K Frequent Element.cs

--- HashMap/Day22-Q347 Top K Frequent Element.cs
+++ HashMap/Day22-Q347 Top K Frequent Element.cs
@@ -3,7 +3,7 @@
 public int[] TopKFrequent(int[] nums, int k) {
     // Create a dictionary to store the frequency of each number.
     Dictionary<int, int> frequencyMap = new Dictionary<int, int>();
-
+    //if input is [1,1,1,1,2,2,3,3,3,4] and k = 2, then frequencyMap will be {1:4, 2:2, 3:3, 4:1}
     // Count the frequency of each number in the array.
     foreach (int num in nums) {
         if (frequencyMap.ContainsKey(num)) {
@@ -13,12 +13,14 @@ public int[] TopKFrequent(int[] nums, int k) {
         }
     }
 
-    // Create a list to store unique numbers.
+    // Create a list to store unique numbers.if input is [1,1,1,1,2,2,3,3,3,4] and k = 2, then uniqueNums will be [1,2,3,4]
     List<int> uniqueNums = frequencyMap.Keys.ToList();
-
+    
     // Sort the unique numbers by their frequency in descending order.
     uniqueNums.Sort((a, b) => frequencyMap[b].CompareTo(frequencyMap[a]));
-
+    //if input is [1,1,1,1,2,2,3,3,3,4] and k = 2, then uniqueNums will be sort as  [1,3,2,4]
     // Take the top K frequent elements from the sorted list.
     return uniqueNums.Take(k).ToArray();
+    //uniqueNums.Take(k) uses the Take method to extract the first K elements from the uniqueNums list. These will be the top K frequent elements because you previously sorted uniqueNums by frequency.
+   //.ToArray() converts the result into an array.
 }

4bd2bc81014cd72a3186f1fa50d0c7ab68cf9783
PENG HE <hopkinau@gmail.com>
2023-09-30 23:51:03 +1000

    Update Q19-Remove Nth Node From End of List.cs

--- Link List/Q19-Remove Nth Node From End of List.cs
+++ Link List/Q19-Remove Nth Node From End of List.cs
@@ -1,14 +1,4 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     public int val;
- *     public ListNode next;
- *     public ListNode(int val=0, ListNode next=null) {
- *         this.val = val;
- *         this.next = next;
- *     }
- * }
- */
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list/
 public class Solution {
     public ListNode RemoveNthFromEnd(ListNode head, int n) {
         var dummy = new ListNode(0, head);
